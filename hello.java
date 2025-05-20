e66d1aafb71e644ef0c13da0a460d8a1ee4dde90
Bhanu09s <bhanu22134@iiitd.ac.in>
2025-05-20 19:31:41 +0530

    Add hello.java

---
+++ hello.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class hello{
+
+    // // instance variable
+    // // non static
+    // public int revise;
+
+    // // static variable
+    // public static int cnt = 0 ;
+
+    // // int i is the local variable
+    // public static void help(int i){
+    //     hello.cnt++;
+    //     System.out.println(i);
+    // }
+    // public static void main(String args[]){
+    //     Scanner in = new Scanner(System.in);
+
+    //     // k is local variables
+    //     int k = in.nextInt();
+
+    //     System.out.println(hello.cnt);
+
+    //     help(k);
+
+    //     System.out.println(hello.cnt);
+
+    //     hello temp = new hello();
+    //     temp.revise = k+cnt;
+
+    //     System.out.println(temp.revise);
+
+    //     System.out.println(k);
+
+    //     in.close();
+
+    public static double sum(int i,int j){
+        return Math.round(5.5);
+    }
+
+    public static void main(String args[]){
+        //Scanner in = new Scanner(System.in);
+        //int n = in.nextInt();
+
+        // for(int i=0;i<n;i++){
+        //     System.out.println(i);
+        // }
+
+        //System.out.println(sum(10,2));
+        //in.close();
+
+        //Type Casting
+        int a = 3;
+        float db = (float)a;
+        System.out.println(db);
+    }
+}
